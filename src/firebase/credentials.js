f8f605be254fc6cf8d6fc35fa97ef3e4eb355758
Nico9934 <nico9934@gmail.com>
2023-03-28 16:05:44 -0300

    Configurando la autenticación con Firebase

--- src/firebase/credentials.js
+++ src/firebase/credentials.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword, connectAuthEmulator } from "firebase/auth"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +19,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const autenticacion = getAuth(app)
+
+//Emulador de autenticacion
+connectAuthEmulator(autenticacion, "local")
+await signInWithEmailAndPassword(autenticacion, email, password)
 
 export const mainCollection = "Users"
 export const teamsCollection = "equipos"
