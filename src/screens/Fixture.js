5f570e5bd936dc3283f760b36192322f4393e0b5
Nicolás <nicolaseugroberto@gmail.com>
2023-03-30 17:33:39 -0300

    Display images

--- src/screens/Fixture.js
+++ src/screens/Fixture.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import { ScrollView, Text, View, Image } from "react-native";
+import appLigasYTorneos from "../firebase/appLigasYTorneos.json";
 export default function Fixture() {
   return (
-    <>
-      
-    </>
-  )
+    <ScrollView>
+      <View>
+        {appLigasYTorneos.map((data) => (
+          <>
+            <Text data={data} key={data.id}>
+              {data.titulo}
+            </Text>
+            <View className={`flex flexrow items-center`}>
+              <Image
+                style={{
+                  resizeMode: "cover",
+                  height: 100,
+                  width: 200,
+                }}
+                source={{ uri: data.logo }}
+              />
+            </View>
+          </>
+        ))}
+      </View>
+    </ScrollView>
+  );
 }
