a7ac2ab3e4ee3ec8b54e9adfbe84eaf78b788f29
niicodeer <nicoradin.job@gmail.com>
2023-04-02 21:29:20 -0300

    added functions to get data

--- src/screens/Fixture.js
+++ src/screens/Fixture.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useRoute, useTheme } from "@react-navigation/native";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { UserContext } from "../context/UserContext";
+import { getFixture } from "../firebase/getFunctions";
 
-export default function Fixture() {
+export default function Fixture({ route }) {
+  const [fixture, setFixture] = useState([]);
+  const { dark, colors } = useTheme();
+  const { user, tournamentId } = useContext(UserContext);
+  const { id } = useRoute().params;
+
+  useEffect(() => {
+    //Me lee el parametro
+    console.log("Id torneo: ", `${id}`);
+    console.log("Id torneo: ", tournamentId);
+
+    //Pero la funcion no me lo toma
+    /* const data = getFixture(user?.id, id); */
+    
+    //Solo cuando paso el id como string literal
+    const data = getFixture(user?.id, "ceyjOmpGpcW9P6EHN6t6");
+    
+    data.then((res) => setFixture(res));
+
+    /*     data.then((res) => setFixture(res));
+    console.log(fixture);
+    console.log(tournamenId); */
+  }, [id]);
+
   return (
-    <>
-      
-    </>
-  )
+    <View className="flex items-center mx-auto justify-center h-full w-full">
+      { fixture.length > 0 ? fixture.map((partido) => (
+        <View
+          className={
+            "flex p-2 px-3 my-1 rounded w-[90%] mx-auto " +
+            (dark ? "bg-slate-700/60" : "bg-black/90")
+          }
+        >
+          <Text className="text-violet-500">Fecha: {partido.fecha}</Text>
+          <Text className="text-white">{partido.equipoLocal}</Text>
+          <Text className="text-yellow-600">vs</Text>
+          <Text className="text-white">{partido.equipoVisitante}</Text>
+        </View>
+      )) : 
+      <View className="flex justify-center items-center">
+        <Text className="text-violet-600">No hay fixture aún</Text>
+      </View>
+      }
+    </View>
+  );
 }
--- src/screens/Ranking.js
+++ src/screens/Ranking.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useNavigation, useTheme } from "@react-navigation/native";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { UserContext } from "../context/UserContext";
+import { getUserTournaments } from "../firebase/getFunctions";
 
 export default function Ranking() {
+  const navigation = useNavigation();
+  const { dark, colors } = useTheme();
+  const { user, setTournamentId } = useContext(UserContext);
+  const [tournaments, setTournaments] = useState([]);
+  useEffect(() => {
+    const data = getUserTournaments(user?.id);
+    data.then((res) => setTournaments(res));
+    console.log(tournaments);
+  }, [user.id]);
   return (
-    <>
-      
-    </>
-  )
+    <View className="flex items-center mx-auto justify-center h-full w-full">
+      {tournaments?.map((tournament) => (
+        <TouchableOpacity
+          key={tournament.id}
+          className={
+            "flex p-2 px-3 my-1 rounded w-[90%] mx-auto " +
+            (dark ? "bg-slate-700/60" : "bg-black/90")
+          }
+          onPress={() => {
+            navigation.navigate({
+              name: "Fixture",
+              params: { id: tournament.id },
+            });
+            setTournamentId(tournament.id);
+          }}
+        >
+          <View>
+            <Text className="text-white text-xl">
+              Torneo: {tournament.nombre}
+            </Text>
+            <Text className="text-white">
+              Lugar: {tournament.direccion}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
 }
