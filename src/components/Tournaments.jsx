cc4478ded35340f3f04e5c9d3a38c61cbfbee5c1
Nicolás <nicolaseugroberto@gmail.com>
2023-04-04 20:16:45 -0300

    width

--- src/components/Tournaments.jsx
+++ src/components/Tournaments.jsx
@@ -6,20 +6,21 @@ export default function Tournaments({ data }) {
     return <Text>Cargando</Text>;
   }
   return (
-    <ScrollView>
+    <ScrollView className="w-full">
       <View>
         {data?.map((data) => (
           <>
             <TouchableOpacity
+              key={data.id}
               onPress={() =>
                 navigation.navigate({
                   name: "VerTorneo",
                   params: { id: data.id },
                 })
               }
-              className="h-16 flex flex-row items-center justify-between border-b-2 bg-gray-100 drop-shadow-sm hover:bg-gray-150 py-[5rem] px-2 rounded mr-1 ml-1 mt-1"
+              className="h-16  flex flex-row items-center justify-between border-b-2 bg-gray-100 drop-shadow-sm hover:bg-gray-150 py-[5rem] px-2 rounded mr-1 ml-1 mt-1"
             >
-              <View className="h-16 flex flex-row items-center justify-between">
+              <View className="flex flex-row items-center justify-between">
                 <Image
                   className="rounded h-12 w-12 m-1"
                   style={{
