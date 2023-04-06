0f052cf638cc373e9a6b8c6b3253b5796bd10384
niicodeer <nicoradin.job@gmail.com>
2023-04-06 17:57:38 -0300

    display teams

--- src/screens/Ranking.js
+++ src/screens/Ranking.js
@@ -1,49 +1,73 @@
 import { useNavigation, useTheme } from "@react-navigation/native";
 import React, { useContext, useEffect, useState } from "react";
-import { View, Text } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { View, Text, Image } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { UserContext } from "../context/UserContext";
-import { getUserTournaments } from "../firebase/getFunctions";
+import {
+  getTournamentTeams,
+  getUserTournaments,
+} from "../firebase/getFunctions";
 
 export default function Ranking() {
   const navigation = useNavigation();
   const { dark, colors } = useTheme();
-  const { user, setTournamentId } = useContext(UserContext);
-  const [tournaments, setTournaments] = useState([]);
+  const { user, tournamentId, createdBy } = useContext(UserContext);
+  const [teams, setTeams] = useState([]);
 
   useEffect(() => {
-    const data = getUserTournaments(user?.id);
-    data.then((res) => setTournaments(res));
+    //Necesito parametros userId, tournamentId para hacer la solicitud, segun el torneo que se solicite
+    if (tournamentId && createdBy ){
 
-    console.log(tournaments);
-  }, [user?.id]);
+      const data = getTournamentTeams(
+        createdBy,
+        tournamentId
+      );
+      data.then((res) => setTeams(res));
+    }
+  }, [tournamentId, createdBy]);
+
   return (
-    <View className="flex items-center mx-auto justify-center h-full w-full">
-      {tournaments?.map((tournament) => (
-        <TouchableOpacity
-          key={tournament.id}
-          className={
-            "flex p-2 px-3 my-1 rounded w-[90%] mx-auto " +
-            (dark ? "bg-slate-700/60" : "bg-black/90")
-          }
-          onPress={() => {
-            navigation.navigate({
-              name: "Fixture",
-              params: { id: tournament.id },
-            });
-            setTournamentId(tournament.id);
-          }}
-        >
-          <View>
-            <Text className="text-white text-xl">
-              Torneo: {tournament.nombre}
-            </Text>
-            <Text className="text-white">
-              Lugar: {tournament.direccion}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      ))}
+    <View className="flex items-center h-full w-full ">
+      <View className="w-full h-36 items-center py-4">
+        <Text className="text-white text-base ">
+          Equipos participantes del torneo:{" "}
+        </Text>
+      </View>
+      <ScrollView className="w-full">
+        <View className="flex flex-row flex-wrap gap-2 items-center justify-center w-full h-auto ">
+          {teams?.map((team) => (
+            <TouchableOpacity
+              key={team.nombre}
+              className={"flex p-2 px-3 my-1 rounded-lg w-32 "}
+              style={{ backgroundColor: colors.lightPrimary }}
+            >
+              <View className="flex felx-col items-center">
+                <Image
+                  source={{ uri: team.imagen }}
+                  className="h-16 w-16 rounded-full "
+                />
+                <Text
+                  className=" text-md my-1"
+                  style={{ color: colors.primaryText }}
+                >
+                  {team.nombre}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
+      {user ? (
+        <View className="my-4">
+          <TouchableOpacity
+            style={{ backgroundColor: colors.accentColor }}
+            className="p-4 rounded-lg"
+            onPress={()=> navigation.navigate("AddTeam")}
+          >
+            <Text>Agregar Equipo</Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
     </View>
   );
 }
