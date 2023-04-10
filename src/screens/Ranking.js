d72956f70f4cab1cb7b47e6bdbafa552d18be373
niicodeer <nicoradin.job@gmail.com>
2023-04-10 00:07:43 -0300

    added loader

--- src/screens/Ranking.js
+++ src/screens/Ranking.js
@@ -1,6 +1,6 @@
 import { useNavigation, useTheme } from "@react-navigation/native";
 import React, { useContext, useEffect, useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ActivityIndicator } from "react-native";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { UserContext } from "../context/UserContext";
 import {
@@ -13,61 +13,65 @@ export default function Ranking() {
   const { dark, colors } = useTheme();
   const { user, tournamentId, createdBy } = useContext(UserContext);
   const [teams, setTeams] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     //Necesito parametros userId, tournamentId para hacer la solicitud, segun el torneo que se solicite
-    if (tournamentId && createdBy ){
-
-      const data = getTournamentTeams(
-        createdBy,
-        tournamentId
-      );
+    if (tournamentId && createdBy) {
+      const data = getTournamentTeams(createdBy, tournamentId);
       data.then((res) => setTeams(res));
     }
+    if(teams.length > 0) setLoading(false)
   }, [tournamentId, createdBy]);
 
   return (
     <View className="flex items-center h-full w-full ">
-      <View className="w-full h-36 items-center py-4">
-        <Text className="text-white text-base ">
-          Equipos participantes del torneo:{" "}
-        </Text>
-      </View>
-      <ScrollView className="w-full">
-        <View className="flex flex-row flex-wrap gap-2 items-center justify-center w-full h-auto ">
-          {teams?.map((team) => (
-            <TouchableOpacity
-              key={team.nombre}
-              className={"flex p-2 px-3 my-1 rounded-lg w-32 "}
-              style={{ backgroundColor: colors.lightPrimary }}
-            >
-              <View className="flex felx-col items-center">
-                <Image
-                  source={{ uri: team.imagen }}
-                  className="h-16 w-16 rounded-full "
-                />
-                <Text
-                  className=" text-md my-1"
-                  style={{ color: colors.primaryText }}
+      {loading ? (
+        <ActivityIndicator size={50} color={colors.accentColor} />
+      ) : (
+        <>
+          <View className="w-full h-36 items-center py-4">
+            <Text className="text-white text-base ">
+              Equipos participantes del torneo:{" "}
+            </Text>
+          </View>
+          <ScrollView className="w-full">
+            <View className="flex flex-row flex-wrap gap-2 items-center justify-center w-full h-auto ">
+              {teams?.map((team) => (
+                <TouchableOpacity
+                  key={team.nombre}
+                  className={"flex p-2 px-3 my-1 rounded-lg w-32 "}
+                  style={{ backgroundColor: colors.lightPrimary }}
                 >
-                  {team.nombre}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          ))}
-        </View>
-      </ScrollView>
-      {user ? (
-        <View className="my-4">
-          <TouchableOpacity
-            style={{ backgroundColor: colors.accentColor }}
-            className="p-4 rounded-lg"
-            onPress={()=> navigation.navigate("AddTeam")}
-          >
-            <Text>Agregar Equipo</Text>
-          </TouchableOpacity>
-        </View>
-      ) : null}
+                  <View className="flex felx-col items-center">
+                    <Image
+                      source={{ uri: team.imagen }}
+                      className="h-16 w-16 rounded-full "
+                    />
+                    <Text
+                      className=" text-md my-1"
+                      style={{ color: colors.primaryText }}
+                    >
+                      {team.nombre}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView>
+          {user ? (
+            <View className="my-4">
+              <TouchableOpacity
+                style={{ backgroundColor: colors.accentColor }}
+                className="p-4 rounded-lg"
+                onPress={() => navigation.navigate("AddTeam")}
+              >
+                <Text>Agregar Equipo</Text>
+              </TouchableOpacity>
+            </View>
+          ) : null}
+        </>
+      )}
     </View>
   );
 }
