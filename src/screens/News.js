2683e416f2dbd673f0b3d11d41c83dbc1bd4f5d6
Nicolás <nicolaseugroberto@gmail.com>
2023-04-10 22:40:08 -0300

    Twitter news

--- src/screens/News.js
+++ src/screens/News.js
@@ -1,12 +1,15 @@
 import Constants from "expo-constants";
 import { Children, useEffect, useState } from "react";
-import { View, Text } from "react-native";
 import { WebView } from "react-native-webview";
 
-export default function News() {
+export default function News({ cuenta }) {
+  /* const [twitter, setTwitter] = useState("DeportesAR"); */
+  const twitter = cuenta ? cuenta : "DeportesAR";
   return (
     <WebView
-      source={{ uri: "https://noticias-torneopalooza.web.app/" }}
+      source={{
+        uri: `https://noticias-torneopalooza.web.app?cuenta=${twitter}`,
+      }}
       style={{ flex: 1 }}
     />
   );
