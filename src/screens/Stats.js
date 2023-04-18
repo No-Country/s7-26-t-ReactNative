06188efd60ae3f5baea144c9e23464de4a09c8fa
Ezequiel <caracido@gmail.com>
2023-04-18 15:48:31 -0300

    feature: create component ErrorPage and Upload in Stats

--- src/screens/Stats.js
+++ src/screens/Stats.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { ErrorPage } from '../components/ErrorPage'
+import { View, TouchableOpacity, Text} from 'react-native'
 
-export default function Stats() {
+
+
+export default function Stats({ navigation }) {
   return (
-    <>
-      
-    </>
+    <View className="flex h-full justify-center items-center">
+      <ErrorPage/>
+
+
+             {/* <TouchableOpacity 
+             style={{ backgroundColor: "#FFC107", width: "55%" }}
+             className="p-3 rounded-xl mb-6 mt-6 shadow-md"
+             onPress={() => navigation.back()}>
+                <Text className="text-gray-800 font-bold text-center text-base">
+                  VOLVER
+                </Text>
+              </TouchableOpacity> */}
+
+    </View>
   )
 }
